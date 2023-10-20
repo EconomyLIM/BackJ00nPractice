4357ce4beae5a02022866ea6cbc9888117855089
EconomyLIM <119987898+EconomyLIM@users.noreply.github.com>
2023-10-20 09:09:41 +0900

    [Silver IV] Title: 학생 번호, Time: 148 ms, Memory: 16092 KB -BaekjoonHub

--- 백준/Silver/1235. 학생 번호/학생 번호.java
+++ 백준/Silver/1235. 학생 번호/학생 번호.java
@@ -1,28 +1,27 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
-        String[] arr = new String[n];
-
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = sc.next();
-        }
-
-        HashSet<String> temp = new HashSet<>();
-
-        for(int i = 1; i <= arr[0].length(); i++){
-            for(int j = 0; j < n; j++){
-                temp.add(arr[j].substring(arr[0].length() - i));
-            }
-            if(temp.size() == n){
-                System.out.println(i);
-                return;
-            }
-            temp.clear();
-        }
-    }
-}
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		String s[] = new String[n];
+		for (int i = 0; i < n; i++) {
+			s[i] = br.readLine();
+		}
+		HashSet<String> hs = new HashSet<>();
+		for (int i = 1; i <= s[0].length(); i++) {
+			for (int j = 0; j < n; j++) {
+				hs.add(s[j].substring(s[0].length()- i));
+			}
+			if (hs.size() == n) {
+				System.out.println(i);
+				return;
+			}
+			hs.clear();
+		}
+	} // main
+} // class

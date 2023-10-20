b453cb560830e518de2675db2e6a7b5fd5587428
EconomyLIM <119987898+EconomyLIM@users.noreply.github.com>
2023-10-20 22:24:53 +0900

    [Silver IV] Title: 바닥 장식, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.java
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.java
@@ -1,23 +1,24 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		int m = scanner.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer nm = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(nm.nextToken());
+        int m = Integer.parseInt(nm.nextToken());
 		char s[][] = new char[n][m];
 		int index = 0;
 		for (int i = 0; i < s.length; i++) {
 			//			String temp = br.readLine();
-			String temp = scanner.next();
+			String temp = br.readLine();
 			for (int j = 0; j < s[0].length; j++) {
 				s[i][j] = temp.charAt(j); 
 
 			} //for
 		} //for
-
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if(i != 0 && s[i][j] == '|' && s[i - 1][j] == '|') {    // |가 위아래로 이어져 있으면 한개로 계산하므로 총 개수를 더해주지 않는다.

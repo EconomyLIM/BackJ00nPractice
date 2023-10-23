b59bdd07e0ffe2fd0283c56793c3660e6286228f
EconomyLIM <119987898+EconomyLIM@users.noreply.github.com>
2023-10-24 08:51:09 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,25 +1,31 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String s = br.readLine();
-		char c [] = new char[s.length()];
-		for (int i = 0; i < c.length; i++) {
-			c[i] = s.charAt(i);
+		int m[] = new int[s.length()];
+		for (int i = 0; i < m.length; i++) {
+			m[i] = s.charAt(i)-'0';
 		} // for
 		
-		int m[] = new int[c.length];
 		for (int i = 0; i < m.length; i++) {
-			m[i] = c[i] -'0';
-		}
-		Arrays.stream(m).boxed().sorted(Collections.reverseOrder()).forEach(System.out::print);
+			int temp;
+			for (int j = i+1; j < m.length; j++) {
+				if (m[i] < m[j]) {
+					temp = m[i];
+					m[i] = m[j];
+					m[j] = temp;
+				}//if
+			}// for
+		} //for
+		
+		for (int i : m) {
+			System.out.print(i);
+		} //for
 		
 	} //main
 } // class

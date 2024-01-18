eca3bdad22b5c2400c8e50fd298776efb6e5c2f7
임경재 <119987898+EconomyLIM@users.noreply.github.com>
2024-01-18 20:48:27 +0900

    [level 2] Title: 주식가격, Time: 14.13 ms, Memory: 60 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public List<Integer> solution(int[] prices) {
+        int[] answer = {};
+        
+        List<Integer> temp = new ArrayList<Integer>();
+        int count = 0;
+        
+        
+        for(int i = 0 ; i < prices.length ; i++){
+            count = 0;
+            for(int j = i ; j < prices.length ; j++){
+                count++;
+                if(prices[i] > prices[j]){
+                    break;
+                }
+            } // for
+            temp.add(count-1);
+        } // for
+        
+        return temp;
+    }
+}

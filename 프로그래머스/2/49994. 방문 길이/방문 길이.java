860abb866ec9948bb289d5ae6ac2979973d161b0
임경재 <119987898+EconomyLIM@users.noreply.github.com>
2024-01-31 00:03:09 +0900

    [level 2] Title: 방문 길이, Time: 27.14 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,12 +1,12 @@
 import java.util.*;
 class Solution {
     public int solution(String dirs) {
-        int answer = 0;
         
         char c [] = dirs.toCharArray();
         HashSet<String> set = new HashSet<>();
         int x = 0, y = 0;
         String currentLine;
+        
         for(int i = 0 ; i < c.length ; i++){
             currentLine = x + "" + y;
             if(c[i] == 'U'){
@@ -14,14 +14,12 @@ class Solution {
                     continue;
                 ++y;
                 set.add(currentLine+x+""+ y+""+x+""+y+currentLine);
-                System.out.println(currentLine+x+""+ y+""+x+""+y+currentLine);
             }
             else if(c[i] == 'D'){
                 if(y-1 < -5)
                     continue;
                 --y;
                 set.add(x+""+y+currentLine+currentLine+x+""+y);
-                System.out.println(x+""+y+currentLine+currentLine+x+""+y);
             }
             else if(c[i] == 'R'){
                 if(x+1 > 5)
@@ -38,8 +36,7 @@ class Solution {
             
             
         } // for
-        
-        
+
         return set.size();
     }
 }

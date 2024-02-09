166bea33ef3861bd2e3e1df491b1bc1b4c015d3e
임경재 <119987898+EconomyLIM@users.noreply.github.com>
2024-02-09 19:58:23 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.25 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -5,20 +5,10 @@ class Solution {
     PriorityQueue<Integer> temp = new PriorityQueue<>( (o1,o2) -> o2-o1 );
     
     public int[] solution(String[] operations) {
-        int[] answer ;
-        
-        // 앞 문자열이 I, D인지
-        
-        // I이면 삽입
-        
-        // D이면 삭제
-        // 비어있으면 continue;
-        
         // init
+        int[] answer ;
         String code;
         int number;
-        LinkedList<Integer> list = new LinkedList<>();
-        int max = Integer.MIN_VALUE;
         
         String line [] ;
         for(int i = 0 ; i < operations.length ; i++){

1c88502203f3c758bc7bea9df5cb7f69aa8d4f08
임경재 <119987898+EconomyLIM@users.noreply.github.com>
2024-02-10 20:11:07 +0900

    [Bronze III] Title: 전자레인지, Time: 140 ms, Memory: 15884 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static final int seconds[] = {360, 60, 10};
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < 3; i++) {
+			int temp = T / seconds[i];
+			sb.append(temp + " ");
+			T = T % seconds[i];
+		} // for
+
+		if (T == 0) {
+			System.out.println(sb.toString());
+		} else {
+			System.out.println("-1");
+		}
+		
+	} // main
+}

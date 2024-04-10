c2453355ae43cad472610260efdc5501966e70ae
임경재 <119987898+EconomyLIM@users.noreply.github.com>
2024-04-11 08:39:45 +0900

    [Silver IV] Title: 올바른 배열, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/1337. 올바른 배열/올바른 배열.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        } // for
+
+        Collections.sort(list);
+        int[] cnt = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < 5; j++) {
+                if (!list.contains(list.get(i) + j)){
+                    cnt[i]++;
+                } //if
+            } // for
+        } // for
+
+        int min = Integer.MAX_VALUE;
+        for (int cnts : cnt){
+            if(cnts < min){
+                min = cnts;
+            }
+        }
+        System.out.println(min);
+
+    }
+}

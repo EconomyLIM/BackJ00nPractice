65a55902e760fd539e7cb9b635f6f139983142a9
임경재 <119987898+EconomyLIM@users.noreply.github.com>
2024-05-22 08:38:31 +0900

    [Silver II] Title: 부분수열의 합, Time: 132 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -4,26 +4,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-        static int cnt = 0, n , s;
-    static int [] map;
-    static boolean [] visited;
+
+    static int n, s, cnt =0;
+    static int []map;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         s = Integer.parseInt(st.nextToken());
 
-        map= new int[n];
-        visited = new boolean[n];
+        map = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             map[i] = Integer.parseInt(st.nextToken());
-        }
+        }// for
+
         dfs(0,0);
 
         if(s == 0) cnt--;
         System.out.println(cnt);
-
     }
     public static void dfs(int index, int sum){
         if(index == n){
@@ -33,7 +33,7 @@ public class Main {
             return;
         }
 
-        dfs(index+1 , sum+ map[index]);
+        dfs(index+1, sum +map[index]);
         dfs(index+1, sum);
     }
 }

e8859cb62084ce5f2e90bb842b7f19e1b0553125
임경재 <119987898+EconomyLIM@users.noreply.github.com>
2024-10-02 12:53:29 +0900

    [Silver V] Title: 다리 놓기, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -2,49 +2,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
 public class Main {
-	
-	private static final int MAX = 31;
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		//init
-		StringTokenizer st;
-		int n = 0 , m = 0;
-		StringBuilder sb = new StringBuilder();
-		int dp [][] = new int[MAX][MAX];
-		
-		
-		for(int i = 1 ; i < MAX ; i++) {
-			dp[i][0] = 1;
-			dp[i][i] = 1;
-		}
-		
-		for (int i = 2; i < MAX; i++) {
-			for (int j = 1; j < MAX; j++) {
-				dp[i][j] = dp[i-1][j-1] + dp[i-1][j]; 
-			}
-		}
-				
-		//impl
-		for (int i = 0; i < T ; i++) {
-			st = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(st.nextToken());
-			m = Integer.parseInt(st.nextToken());
+    
+    static final int MAX = 31; 
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        int [][] dp = new int[MAX][MAX];
+        
+        // init 
+        init(dp);
 
-			if(i != T-1) {
-				sb.append(dp[m][n]).append("\n");
-			}else {
-				sb.append(dp[m][n]);
-			}
-			
-			
-		} // for
+        // dp impl
+        dpImpl(dp);
 
-		
-		System.out.println(sb.toString());
-	} 
-} // class
+        // input
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st ;
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            sb.append(dp[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())]).append("\n");
+        }
+
+        System.out.println(sb.toString().substring(0, sb.toString().length() - 1));
+    }
+
+    private static void dpImpl(int[][] dp) {
+        for (int i = 2; i < MAX; i++) {
+            for (int j = i+1; j < MAX; j++) {
+                dp[i][j] = dp[i][j-1] + dp[i-1][j-1];
+            }
+        }
+    }
+
+    private static void init(int[][] dp) {
+        for (int i = 0; i < MAX; i++) {
+            dp[1][i] = i;
+            dp[i][i] = 1;
+        } // for
+    }
+}

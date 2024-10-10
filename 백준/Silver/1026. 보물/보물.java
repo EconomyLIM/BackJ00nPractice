6f72b5301bbeaeab7c914ca29ac5b69d0abbe4a6
임경재 <119987898+EconomyLIM@users.noreply.github.com>
2024-10-10 09:14:57 +0900

    [Silver IV] Title: 보물, Time: 100 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -2,38 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st ;
-		
-		Integer a [] = new Integer[n];
-		Integer b [] = new Integer[n];
-		
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < a.length; j++) {
-				a[j] = Integer.parseInt(st.nextToken());
-			}
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < a.length; j++) {
-				b[j] = Integer.parseInt(st.nextToken());
-			}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer N = Integer.parseInt(br.readLine());
+        int []A = new int[N];
+        int []B = new int[N];
 
-		Arrays.sort(a);
-		Arrays.sort(b, Collections.reverseOrder());
-		
-		int sum = 0;
-		for (int i = 0; i < a.length; i++) {
-			
-			sum += a[i] * b[i];
-			
-		}
-		
-		System.out.println(sum);
-	} // main
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer += A[i] * B[N-1-i];
+        }
+
+        System.out.println(answer);
+    }
 }

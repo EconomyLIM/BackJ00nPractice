486b57bb035d6b32a68801728e448caa996c9ff7
임경재 <119987898+EconomyLIM@users.noreply.github.com>
2024-10-31 11:43:41 +0900

    [Gold IV] Title: 동전 1, Time: 116 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -4,25 +4,23 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, k;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        int [] arr = new int[k+1];
-        int [] dp = new int[k+1];
+        int [] arr = new int[n];
+        long [] dp = new long[k+1];
 
         dp[0] = 1;
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
             for (int j = arr[i]; j <= k; j++) {
                 dp[j] += dp[j - arr[i]];
-            }
+            } // for
         }
 
         System.out.println(dp[k]);
-
     }
 }

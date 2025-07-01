1c7a7bbe8661388ca1c8731bad3c3464f989bb60
임경재 <119987898+EconomyLIM@users.noreply.github.com>
2025-07-01 12:47:07 +0900

    [level 1] Title: 달리기 경주, Time: 393.85 ms, Memory: 379 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -1,28 +1,30 @@
 import java.util.*;
-
+import java.util.stream.Collectors;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        
-        HashMap<String, Integer> map = new HashMap<>();
-          
-        for(int i = 0 ; i < players.length ; i++){
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0 ; i < players.length ; i ++){
             map.put(players[i], i);
+        }
+        
+        String prevCallingName = "";
+        String callingName = "";
+        int callingNameLevel = 0;
+        
+        for(int i = 0 ; i < callings.length ; i ++ ){
+            callingNameLevel = map.get(callings[i]);
+            prevCallingName = players[callingNameLevel-1];
+            callingName = callings[i];
+        
+            players[callingNameLevel-1] = callingName;
+            players[callingNameLevel] = prevCallingName;
             
-        } // for
-        
-        int ranking;
-        String frontPlayer;
-        
-         for(String s : callings){
-             ranking = map.get(s); // 이름 불린 사람의 순위를 갖고온다.
-             frontPlayer = players[ranking-1]; // 찾은 사람의 바로 앞사람 이름을 갖고온다
-             map.replace(s, ranking-1); // 이름 불린 사람의 순위를 1 감소 시킨다.
-             map.replace(frontPlayer, ranking); // 바로 앞사람의 순위를 1 증가시킨다(찾은 순위로 바꾼다).
-             
-             players[ranking-1] = s; // 앞사람의 이름을 바꾼다
-             players[ranking] = frontPlayer; // 찾은 사람의 이름을 바꾼다
-             
-         }
+            // 이전에 있던 사람 순위는 불린 사람의 순위대로
+            map.put(prevCallingName, callingNameLevel);
+            
+            // 불린 사람의 이전 사람의 순위로 이동
+            map.put(callings[i], callingNameLevel - 1);
+        }
 
         return players;
     }

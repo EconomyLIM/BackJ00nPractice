d3815c5b5f9679aa8d570e83e0230be630da3e70
임경재 <119987898+EconomyLIM@users.noreply.github.com>
2025-07-04 12:24:30 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,49 +1,18 @@
-import java.util.*;
-
 class Solution {
-    static Stack<Integer> cards1IdxList = new Stack<>();
-    static Stack<Integer> cards2IdxList = new Stack<>();
-    
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
-        String temp;
-
-        for(int i = 0 ; i < goal.length ; i ++){
-            temp = goal[i];
-            
-            for(int j = 0 ; j < cards1.length ; j++){
-                if(temp.equals(cards1[j])){
-                   // System.out.println("card1: " + cards1[j] + j);
-                    if( checkIdx(cards1IdxList, j) ){
-                        return "No";
-                    } else{
-                        continue;
-                    }
-                } // if
+        
+        int idx = 0, idx2 = 0;
+        
+        for(String word: goal){
+            if(idx < cards1.length && cards1[idx].equals(word)){
+                idx++;   
+            }else if(idx2 < cards2.length && cards2[idx2].equals(word)){
+                idx2++;
+            }else{
+                return "No";
             }
-            
-            for(int z = 0 ; z < cards2.length ; z++){
-                if(temp.equals(cards2[z])){
-                    if( checkIdx(cards2IdxList, z) ){
-                        return "No";
-                    } else{
-                        continue;
-                    }
-                } // if
-            } // for
-            
-        } // for
-
+        }
+        
         return "Yes";
-    } // solution
-    
-    public static boolean checkIdx(Stack<Integer> stack, int idx){
-        
-        if(!stack.isEmpty() && stack.peek()+1 != idx){
-            return true;
-        } // if
-        stack.push(idx);
-        
-        return false;
-    }//checkIdx
-} // class
+    }
+}
